48d1302171d446a33d3d441db453d4f5134e2890
Andrey <81797774+Aiizbar@users.noreply.github.com>
2021-12-21 17:39:02 +0500

    Update block.py

--- block.py
+++ block.py
@@ -13,6 +13,6 @@ class Platform(pg.sprite.Sprite):
     def __init__(self, x, y):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-        self.image = pg.image.load("../IMAGE_GAME/IMAGE_MAP/TREE.png")
+        self.image = pg.image.load("TREE.png")
         # self.image.fill(pg.Color(PLATFORM_COLOR))
         self.rect = pg.Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
